5812b5fe5a2c0aa5a7431c840866a676dd75ff27
Kacper Golinski <kacper.golinski@gmail.com>
2022-11-08 19:18:34 +0100

    Authenticate and print S3 buckets

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS CDK Hello Rust
+
+App based on https://www.serverlessguru.com/blog/aws-sdk-for-rust-getting-started
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use aws_sdk_s3::{Client, Error};
+
+async fn print_buckets() -> Result<(), Error> {
+    let config = aws_config::load_from_env().await;
+
+    // create an s3 client
+    let s3 = Client::new(&config);
+
+    // list the first page of buckets in the account
+    let response = s3.list_buckets().send().await?;
+
+    if let Some(buckets) = response.buckets() {
+        for bucket in buckets {
+            println!("bucket name: {}", bucket.name().unwrap());
+        }
+    } else {
+        println!("You don't have any buckets!");
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    print_buckets().await
 }
