d1b42a22fc533532d3f57ffb274cc5391d0c9da2
Kacper Golinski <kacper.golinski@gmail.com>
2022-11-09 18:23:10 +0100

    upload file to the bucket

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
 use aws_sdk_s3::{
     model::{BucketLocationConstraint, CreateBucketConfiguration},
+    types::ByteStream,
     Client, Error, Region,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let bucket_region = "us-east-2";
+    let bucket_name = "hello-rust-bucket-1";
+
+    let region_provider = Region::new(bucket_region);
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let s3 = Client::new(&config);
+
+    let file_key = "plaintext.txt";
+    let file_body = ByteStream::from_static("Hello".as_bytes());
+
+    let result = s3
+        .put_object()
+        .bucket(bucket_name)
+        .key(file_key)
+        .body(file_body)
+        .content_type("text/plain")
+        .send()
+        .await;
+
+    match result {
+        Ok(_) => {
+            println!("Succesfully uploaded {file_key} to {bucket_name}");
+        }
+
+        Err(err) => {
+            eprintln!("Error uploading {file_key} {err}");
+        }
+    }
+
     Ok(())
 }
 
