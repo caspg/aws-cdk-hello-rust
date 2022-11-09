f84e619a47f3b72f3df7334dc042d14c3ae60b2c
Kacper Golinski <kacper.golinski@gmail.com>
2022-11-09 18:15:05 +0100

    create buckets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use aws_sdk_s3::{Client, Error};
+use aws_sdk_s3::{
+    model::{BucketLocationConstraint, CreateBucketConfiguration},
+    Client, Error, Region,
+};
 
 async fn print_buckets() -> Result<(), Error> {
+    // get default credentials from ~/.aws/credentials
     let config = aws_config::load_from_env().await;
 
     // create an s3 client
@@ -22,5 +26,29 @@ async fn print_buckets() -> Result<(), Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    print_buckets().await
+    let bucket_region = "us-east-2";
+    let bucket_name = "hello-rust-bucket-1";
+
+    let region_provider = Region::new(bucket_region);
+    let config = aws_config::from_env().region(region_provider).load().await;
+
+    let s3 = Client::new(&config);
+
+    println!("Creating {bucket_name} in {bucket_region}");
+
+    let constraint = BucketLocationConstraint::from(bucket_region);
+
+    let bucket_configuration = CreateBucketConfiguration::builder()
+        .location_constraint(constraint)
+        .build();
+
+    s3.create_bucket()
+        .create_bucket_configuration(bucket_configuration)
+        .bucket(bucket_name)
+        .send()
+        .await?;
+
+    println!("Successfull created {bucket_name}");
+
+    Ok(())
 }
